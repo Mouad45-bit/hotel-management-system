83cb3d4ae40ab1c5f7bb51b70a416ed78b74562e
mouad45-bit <mouadfekkak2002@gmail.com>
2026-05-15 01:03:08 +0100

    HMS-8 enable config server

--- config-server/src/main/java/com/hotel/management/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/hotel/management/configserver/ConfigServerApplication.java
@@ -2,8 +2,17 @@ package com.hotel.management.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Point d'entrée du config-server.
+ *
+ * Ce service centralise les fichiers de configuration des microservices.
+ * Les futurs services comme auth-service, eureka-server et api-gateway
+ * viendront récupérer leurs fichiers YAML ici au démarrage.
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
