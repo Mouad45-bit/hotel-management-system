62a3f7bcfd326c0361edf37dc69fd0bedfaa7e33
mouad45-bit <mouadfekkak2002@gmail.com>
2026-05-16 01:05:06 +0100

    HMS-25 add Eureka client to api-gateway

--- api-gateway/src/main/java/com/hotel/management/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hotel/management/apigateway/ApiGatewayApplication.java
@@ -2,14 +2,19 @@ package com.hotel.management.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Point d'entrée de l'API Gateway.
  *
  * Ce microservice sera le point d'entrée unique de l'application.
  * Les clients externes n'appelleront pas directement les services internes.
+ *
+ * @EnableDiscoveryClient permet à la Gateway de s'enregistrer dans Eureka
+ * et d'utiliser les noms logiques des services comme lb://auth-service.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
