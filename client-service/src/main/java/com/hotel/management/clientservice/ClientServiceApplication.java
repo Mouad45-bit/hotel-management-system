ee340b1adff174cc00f856ded5fc7b0943b473bd
MRdrissi <marwane.drissi.ben@gmail.com>
2026-06-23 19:48:53 +0100

    CommandeLineRunner dans la class ClientServiceApplication , pour des butes de comprehension on l'enleve plus tard

--- client-service/src/main/java/com/hotel/management/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/com/hotel/management/clientservice/ClientServiceApplication.java
@@ -1,13 +1,30 @@
 package com.hotel.management.clientservice;
 
+import com.hotel.management.clientservice.entity.Client;
+import com.hotel.management.clientservice.repository.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ClientServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ClientRepository clientRepository) {
+        return args -> {
+            Stream.of("Abbas" ,"achraf","nabil","boutaina","ilham").forEach(name -> {
+                clientRepository.save(Client.builder().firstName(name).lastName(name).cin(String.valueOf(10+Math.random()*10)).build());
+            });
+
+            clientRepository.findAll().forEach(System.out::println);
+        };
     }
 }
